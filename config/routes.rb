df00095f6096fca70b0d43b4030f92057b005582
natty-mii <n.miya7@gmail.com>
2020-11-11 17:34:00 +0900

    add to code in config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    resgistration: 'users/registrations'
+    resgistration: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   devise_scope :user do
     get 'registrations', to: 'users/registrations#create_users'
